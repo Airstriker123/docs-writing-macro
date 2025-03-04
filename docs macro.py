48ad2ec9ea9c61a172a9b59400931190de26e693
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2025-03-04 18:30:39 +1100

    Update docs macro.py

--- docs macro.py
+++ docs macro.py
@@ -1,15 +1,13 @@
 import time
 import pyautogui
 
-# The text to be typed into Google Docs
+
 text = input('Enter you text:')
-# Wait a few seconds before starting to give you time to switch to Google Docs
+
 print("Switch to Google Docs and place your cursor in the document. Typing will start in 5 seconds...")
-time.sleep(5)
+time.sleep(5) #5 seconds until macro starts
 
-# Type the text with a slight delay between characters
 for line in text.split("\n"):
-    pyautogui.write(line, interval=0.1)  # Adjust typing speed (0.02 = 20ms delay per character)
-    pyautogui.press("enter")  # Simulate pressing Enter at the end of each paragraph
-
+    pyautogui.write(line, interval=0.1)  
+    pyautogui.press("enter") 
 print("Typing complete!")
